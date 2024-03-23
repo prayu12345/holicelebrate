bea5fd05d7c6abb6f93040f04c579efd5f0d5320
prayu12345 <164336616+prayu12345@users.noreply.github.com>
2024-03-23 19:53:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// Function to send Holi wishes
+function sendWishes() {
+    window.open("file:///C:/Users/akhil/OneDrive/Desktop/wishing/xxxx.html"); // Display a customized Holi wish message
+}
+
+// Function to play color splash animation
+function playColorSplashAnimation() {
+    // You can implement an animation to simulate color splashes on the webpage
+    // For example, change background color, animate elements, etc.
+    window.open("file:///C:/Users/akhil/OneDrive/Desktop/wishing/colorsplash/colorsplash.html"); 
+}
+
+// Function to open Holi-themed playlist in a new tab or window
+function openHoliPlaylist() {
+    window.open("file:///C:/Users/akhil/OneDrive/Desktop/wishing/playlist/playlist.html", "_blank");
+}
+
+// Function to share Holi wishes on social media
+function shareOnSocialMedia() {
+    alert("Sharing on Social Media!"); // Implement logic to share the Holi wishes on social media platforms
+}
